5ac72499715f07321e876d41405cef1239fb15d2
Sam <71229731+2Amateurs@users.noreply.github.com>
2022-10-17 14:26:33 -0400

    new trajectory class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectory/Trajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/trajectory/Trajectory.java
@@ -1,4 +1,26 @@
 package org.firstinspires.ftc.teamcode.trajectory;
 
+import org.firstinspires.ftc.teamcode.geometry.Pose2D;
+
+import java.util.ArrayList;
+
 public class Trajectory {
+    private ArrayList<Pose2D> waypoints = new ArrayList();
+    private Trajectory(Builder builder){
+        this.waypoints = builder.waypoints;
+    }
+    public static class Builder {
+        private Builder() {}
+        private ArrayList<Pose2D> waypoints = new ArrayList();
+        public Builder setWaypoint(Pose2D waypoint) {
+            this.waypoints.add(waypoint);
+            return this;
+        }
+        public Trajectory build() {
+            return new Trajectory(this);
+        }
+    }
+    public static Builder getBuilder() {
+        return new Builder();
+    }
 }
